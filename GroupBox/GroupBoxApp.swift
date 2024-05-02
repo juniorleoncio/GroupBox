625a1f67b5396ba670861cafa5993c57c0bc2dd4
Junior Leoncio <graciano.leoncio@gmail.com>
2024-05-02 18:22:05 -0300

    Initial Commit

---
+++ GroupBox/GroupBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupBoxApp.swift
+//  GroupBox
+//
+//  Created by Junior Leoncio on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GroupBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
