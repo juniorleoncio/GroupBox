9b56e8a9e7204ca11cb3be49528bd6448e49d6d2
Junior Leoncio <graciano.leoncio@gmail.com>
2024-05-02 23:13:20 -0300

    initial commit

--- GroupBox/ContentView.swift
+++ GroupBox/ContentView.swift
@@ -9,16 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            Rectangle()
+                .foregroundStyle(.pink.gradient.opacity(0.8))
+                .ignoresSafeArea()
+            VStack(spacing: 40) {
+                GroupBox {
+                    Text("Este é meu groupbox. Vamos usar com um Card View ")
+                }
+                .frame(width: 240)
+                .groupBoxStyle(.music)
+                
+                GroupBox {
+                    GroupBox {
+                        MusicPlayerView()
+                    }
+                    
+                } label: {
+                    Label("Tocando Agora", systemImage: "music.note")
+                    
+                }
+                .groupBoxStyle(.music)
+            }
+            .padding()
         }
-        .padding()
+        
+       
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct MusicGroupBoxStyle: GroupBoxStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        VStack(alignment: .leading) {
+            configuration.label
+                .bold()
+                .fontDesign(.monospaced)
+                .foregroundStyle(.pink)
+            configuration.content
+        }
+        .padding()
+        .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 12))
+    }
+}
+
+extension GroupBoxStyle where Self == MusicGroupBoxStyle {
+    static var music: MusicGroupBoxStyle { .init() }
+}
+
+struct MusicPlayerView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .frame(width: 50, height: 50)
+                    .foregroundStyle(.secondary)
+                
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Cool Song Title")
+                        .font(.footnote)
+                        .foregroundStyle(.secondary)
+                }
+                
+                Spacer()
+            }
+            .padding(.bottom, 8)
+            
+            ProgressView(value: 0.4, total: 1)
+                .tint(.secondary)
+                .padding(.bottom, 20)
+            
+            HStack(spacing: 30) {
+                Image(systemName: "backward.fill")
+                Image(systemName: "pause.fill")
+                Image(systemName: "forward.fill")
+            }
+            .foregroundStyle(.secondary)
+            .font(.title)
+        }
+    }
 }
